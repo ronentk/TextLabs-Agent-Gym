d204d0198dc23d665e997e6afbdee23b2e0da9c7
Ronen Tamari <ronen.tamari1@mail.huji.ac.il>
2019-04-04 11:14:53 +0300

    gitignore

---
+++ textlabs_agents_gym_setup/textlabs_agents_gym/configs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from pathlib import Path
+
+root_path = Path('/home/ronent/dev/phd/ttq/TextLab-Agents-Gym/textlabs_agents_gym_setup/textlabs_agents_gym')
+data_path = root_path.parent.parent / 'data'
+generated_games_dir = data_path / 'gen_games'
